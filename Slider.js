784ceaf42b1ddb3ced2c85834226a206d8ac9532
Junyoung Park <ssam1221@naver.com>
2021-12-16 19:28:58 +0900

    Fix ending text line

--- Slider.js
+++ Slider.js
@@ -219,7 +219,7 @@ const Slider = (() => {
                 this.addText({
                     x: `0`,
                     y: `80vh`,
-                    text: '<span class="PenStyle" style="font-size: 96px;">End of page.</span><br><br>' +
+                    text: '<span class="PenStyle" style="font-size: 96px;">End of page.</span><br>' +
                         '<span style="font-size: 32px;">이 페이지는 바닐라 스크립트로 제작되었습니다.</span>',
                     style: {
                         color: `white`,

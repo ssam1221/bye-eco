5b8d4c3dafddec4b17efb7092ea5d0e294bcc105
Junyoung Park <ssam1221@naver.com>
2021-12-17 15:31:10 +0900

    Add yerim

--- customized/yerim.ju.js
+++ customized/yerim.ju.js
@@ -6,128 +6,146 @@ document.getElementById(`startAnimation`).onclick = () => {
 
     Slider.init();
 
-    Slider.addAudio({
-        src: `A Fallen Leaf.mp3`,
-        start: 1000,
-        duration: 50000
-    })
-
-    // Slider.addAudio({
-    //     src: `In a Fluffy Cotton Candy.mp3`,
-    //     start: 5000,
-    //     duration: 5000
-    // })
 
     let _textStart = 1000;
+
     function getTextStart() {
         _textStart += 4000;
         return _textStart;
     }
 
+    function getImageStart() {
+        return _textStart;
+    }
+
     Slider.addText({
-        text: '안녕하세요.',
+        text: '안녕하세요. 예림씨.',
         center: true,
         style: {
             color: `white`,
         },
-        start: _textStart, duration: 3000,
+        start: _textStart,
+        duration: 3000,
     });
 
     Slider.addText({
-        text: '12월 3일을 마지막으로<br>서초로 출근하는<br>박준영 선임입니다.',
+        text: '접니다.',
         center: true,
         style: {
             color: `white`
         },
-        start: getTextStart(), duration: 3000,
-    });
-
-
-    Slider.addImage({
-        x: `calc(50vw - 250px)`, y: `25vh`,
-        src: 'webossignage_logo.png',
-        style: {
-        },
-        start: 9000, duration: 3000,
+        start: getTextStart(),
+        duration: 3000,
     });
 
     Slider.addText({
-        text: '19일 부로 14년부터 해 왔던<br>webOS Signage 업무에서,',
+        text: '평범하게 메일로만 인사하는 건 제 취향이 아닌거같아서',
         center: true,
         style: {
             color: `white`
         },
-        start: getTextStart(), duration: 3000,
+        start: getTextStart(),
+        duration: 3000,
     });
 
-    Slider.addImage({
-        x: `calc(50vw - 235px)`, y: `25vh`,
-        src: 'procentric_logo.png',
+    Slider.addText({
+        text: '이런거 만들어봤습니다.',
+        center: true,
         style: {
+            color: `white`
         },
-        start: 13000, duration: 3000,
+        start: getTextStart(),
+        duration: 3000,
+    });
+
+    Slider.addText({
+        text: '예림씨도 입사한 이후에 이것저것 많은 일들을 했던 것 같은데',
+        center: true,
+        style: {
+            color: `white`
+        },
+        start: getTextStart(),
+        duration: 3000,
+    });
+
+    Slider.addText({
+        text: '특히 같이 야근하면서 NDK 했던 건 아직도 기억에 남네요.',
+        center: true,
+        style: {
+            color: `white`
+        },
+        start: getTextStart(),
+        duration: 3000,
+    });
+
+    Slider.addText({
+        text: '개인적으로 올해 초에 DX로 가신 건 정말 잘한 선택인 것 같아요.',
+        center: true,
+        style: {
+            color: `white`
+        },
+        start: getTextStart(),
+        duration: 3000,
+    });
+
+    Slider.addText({
+        text: '여기 있었으면 개발업무도 제대로 못했을테고',
+        center: true,
+        style: {
+            color: `white`
+        },
+        start: getTextStart(),
+        duration: 3000,
+    });
+
+    Slider.addText({
+        text: '개발자사이트 한다고 하면 인정도 제대로 못 받았을 것 같았으니까요.',
+        center: true,
+        style: {
+            color: `white`
+        },
+        start: getTextStart(),
+        duration: 3000,
+    });
+
+    Slider.addText({
+        text: '앞으로도 본인의 주력 역량 꾸준히 키워서',
+        center: true,
+        style: {
+            color: `white`
+        },
+        start: getTextStart(),
+        duration: 3000,
+    });
+
+    Slider.addText({
+        text: '전문가로 인정받고 다녔으면 좋겠습니다.',
+        center: true,
+        style: {
+            color: `white`
+        },
+        start: getTextStart(),
+        duration: 3000,
     });
 
     Slider.addText({
         center: true,
-        text: '새롭게 클라우드솔루션의<br>Pro:Centric 업무로 이동합니다.',
+        text: '그리고 서초로 납치되기 전에 시간 나면<br>소연씨랑 같이 종종 커피타임 가지면 좋겠네요.',
         style: {
             color: `white`
         },
-        start: getTextStart(), duration: 3000,
+        start: getTextStart(),
+        duration: 3000,
     });
 
     Slider.addText({
         center: true,
-        text: '오랜 기간동안 하던 업무 대신 새로운 업무',
+        text: 'YB모임때 또다시 보죠.',
         style: {
             color: `white`
         },
-        start: getTextStart(), duration: 3000,
-    });
-
-    Slider.addText({
-        center: true,
-        text: '오랜 기간동안 하던 업무 대신 새로운 업무',
-        style: {
-            color: `white`
-        },
-        start: getTextStart(), duration: 3000,
-    });
-
-
-
-
-
-
-    Slider.addText({
-        center: true,
-        text: '지금까지 같이 일한 ',
-        style: {
-            color: `white`
-        },
-        start: getTextStart(), duration: 3000,
-    });
-
-
-
-
-    Slider.addText({
-        center: true,
-        text: '또한 1년 가량 Web Solution 프로젝트에 있으면서,',
-        style: {
-            color: `white`
-        },
-        start: getTextStart(), duration: 3000,
-    });
-
-    Slider.addText({
-        center: true,
-        text: '여러모로 도와주신 분들께 감사 드립니다.',
-        style: {
-            color: `white`
-        },
-        start: getTextStart(), duration: 3000,
+        start: getTextStart(),
+        duration: 3000,
     });
 
     Slider.addText({
@@ -136,8 +154,16 @@ document.getElementById(`startAnimation`).onclick = () => {
         style: {
             color: `white`
         },
-        start: getTextStart(), duration: 3000,
+        start: getTextStart(),
+        duration: 3000,
     });
 
+    Slider.end(getTextStart());
+
+    Slider.addAudio({
+        src: `Night Beach Memories.mp3`,
+        start: 1000,
+        duration: getTextStart()
+    });
     Slider.play();
 }
